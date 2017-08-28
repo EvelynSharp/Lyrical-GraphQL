b122ec4b49301e1466cbf00f419cbcded3306dd5
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-28 14:14:53 -0600

    add mongo

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
@@ -8,7 +9,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PW}@ds161443.mlab.com:61443/lyricaldb-es`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
